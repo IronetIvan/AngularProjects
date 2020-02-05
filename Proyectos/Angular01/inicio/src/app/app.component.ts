a56c7d0e987787e244891265b8958ffae45355dd
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-02-05 12:10:07 +0100

    angular iconos

--- Proyectos/Angular01/inicio/src/app/app.component.ts
+++ Proyectos/Angular01/inicio/src/app/app.component.ts
@@ -12,8 +12,18 @@ export class AppComponent implements Object{
   nombre:string = "Ivan Redondo";
   rutajava:string = "../assets/images/java.png";
   objetoJava:Tecnologia = new Tecnologia("Java", "Plataforma desarrolada por Orcale", this.rutajava);
+  objetoPS:Tecnologia = new Tecnologia("PowerShell", "Automatizacion de SO", "../assets/images/powershell.png");
+  objetoHtml:Tecnologia = new Tecnologia("HTML", "Aplicaciones web", "../assets/images/html.jpeg");
+  objetoMysql:Tecnologia = new Tecnologia("MySql", "Administracion de base de datos", "../assets/images/mysql.jpg");
+objetoPhp:Tecnologia = new Tecnologia("PHP", "Desarrollado para webs dinamicas", "../assets/images/php.png");
+objetoPy:Tecnologia = new Tecnologia("Python", "Desarrollado para backend", "../assets/images/python.jpg");
+objetoAngular:Tecnologia = new Tecnologia("Angular", "Multiplataforma desarrollada por Google", "../assets/images/angular-icon.svg");
+objetoJScript:Tecnologia = new Tecnologia("JavaScript", "Backend y Frontend", "../assets/images/js.jpg");
+objetoXml:Tecnologia = new Tecnologia("XML", "Estructura de datos", "../assets/images/xml.jpg");
+elementos:Tecnologia[] = [this.objetoJava, this.objetoAngular, this.objetoHtml, this.objetoMysql, this.objetoPS, this.objetoPhp, this.objetoPy, this.objetoXml ]
 
-ngOnInit():void{
+
+/*ngOnInit():void{
   console.log(this.objetoJava.getTitulo());
-}
+}*/
 }
