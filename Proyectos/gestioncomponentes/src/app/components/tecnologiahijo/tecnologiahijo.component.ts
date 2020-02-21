3bb321ed5def4f6451ffd4a48d4d56d14e8f5010
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-02-21 13:32:01 +0100

    act

---
+++ Proyectos/gestioncomponentes/src/app/components/tecnologiahijo/tecnologiahijo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologiahijo',
+  templateUrl: './tecnologiahijo.component.html',
+  styleUrls: ['./tecnologiahijo.component.css']
+})
+export class TecnologiahijoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
